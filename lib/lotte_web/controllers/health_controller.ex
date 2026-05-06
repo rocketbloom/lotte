fa29d27afcce6988f5e57fb76e88c9a410b97e3e
Melle Lieuwes <m.lieuwes@eyra.co>
2026-05-06 15:49:29 +0200

    Add version field to health endpoint to test CI/CD

--- lib/lotte_web/controllers/health_controller.ex
+++ lib/lotte_web/controllers/health_controller.ex
@@ -2,6 +2,6 @@ defmodule LotteWeb.HealthController do
   use LotteWeb, :controller
 
   def check(conn, _params) do
-    json(conn, %{status: "ok", service: "lotte"})
+    json(conn, %{status: "ok", service: "lotte", version: "1"})
   end
 end
